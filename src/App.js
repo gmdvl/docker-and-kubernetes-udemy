155e3c967519fcbfeb183ed79a16a334130794cd
gmdvl <gabrielmdvl@gmail.com>
2021-11-28 22:26:56 -0400

    jenkins build test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>demo there!</p>
+        <p>YESS there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
